5e566c5b8491da9f5600c822961c56128557031d
Luc <luc@lucemans.nl>
2026-06-29 11:00:31 +0200

    Add flake file

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "wallet.page devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+          pnpm_11
+        ];
+
+        shellHook = ''
+        '';
+      };
+    });
+}
